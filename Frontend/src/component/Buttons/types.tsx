e6dc4e580de13321c7ef32cf012ab0fc01c87e84
RadonZ <supachai27102000@gmail.com>
2023-05-08 11:19:25 +0700

    can't use

--- Frontend/src/component/Buttons/types.tsx
+++
@@ -1,9 +0,0 @@
-import {ReactNode} from 'react';
-import {StyleProp,ViewStyle,TextStyle,GestureResponderEvent} from 'react-native';
-
-export interface ButtonProps{
-    children:ReactNode;
-    style?: StyleProp<ViewStyle>;
-    textStyle?: StyleProp<TextStyle>;
-    onPress: ((event: GestureResponderEvent) => void) | undefined
-}
