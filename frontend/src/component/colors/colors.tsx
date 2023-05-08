a83cadf253c0a5a12d5be4c94d6477857d3f9468
RadonZ <supachai27102000@gmail.com>
2023-05-08 13:48:16 +0700

    add colors

---
+++ frontend/src/component/colors/colors.tsx
@@ -0,0 +1,26 @@
+export const colors = {
+    primary: "#24C16B",
+    secondary: "#82CAFA",
+
+    green: "#66D59A",
+    lightgreen: "#E6FEF0",
+
+    lime: "#00BA63",
+    emerald: "#2BC978",
+
+    darkblue:"#0081A7",
+
+    white: "#FFFFFF",
+
+    lightgrey: "#FCBFC",
+    gray: "#C1C3C5",
+    darkgray:"#C3C6C7",
+
+    pearlblue: "#012F66",
+    lightblue:" #ADD8E6",
+    bluesky: "#87CEEB",
+
+    black:"#000000",
+
+    transparent:"transparent",
+}
