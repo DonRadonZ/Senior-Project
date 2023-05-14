e82535d1588e13e1c450c338478eed9968e16f03
RadonZ <supachai27102000@gmail.com>
2023-05-14 23:28:58 +0700

    remove unused color

--- frontend/src/Components/Colors/colors.tsx
+++ frontend/src/Components/Colors/colors.tsx
@@ -1,11 +1,12 @@
 export const colors = {
-    darkblue:"#0081A7",
+
     malibu1:"#4FACFE",
     malibu2:"#00f2fe",
     white: "#FFFFFF",
     gray: "#808080",
     black: "#000000",
     navyblue: "#000080",
+    darkblue: "092147",
 
     transparent:"transparent",
 };
