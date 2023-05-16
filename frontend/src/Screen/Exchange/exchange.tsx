8d78e7137d562c1a15cf2157c9b6fa575e6a26ba
RadonZ <supachai27102000@gmail.com>
2023-05-17 01:13:35 +0700

    add function component

--- frontend/src/Screen/Exchange/exchange.tsx
+++ frontend/src/Screen/Exchange/exchange.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{FunctionComponent} from 'react';
 import {ImageBackground} from 'react-native'; 
 // import MainContainer from '../../component/Container/MainContainer';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -8,12 +8,18 @@ import BuyChoiceButton from '../../Components/Button/BuyChoiceButton';
 import SellChoiceButton from '../../Components/Button/SellChoiceButton';
 import styles from '../../Components/Container/Backgroundstyle';
 import { createStackNavigator } from '@react-navigation/stack';
+import { IStackScreenProps } from '../../Components/library/StackScreenProps';
+
 
 
 
 const Stack = createStackNavigator()
 
-export const Exchange = () =>{
+const Exchange: FunctionComponent<IStackScreenProps> = (props) => {
+  
+  const { navigation, route } = props;
+
+
   return(
     <ImageBackground
     source={require("../../../assets/Background-image.jpg")}
@@ -25,3 +31,4 @@ export const Exchange = () =>{
   )
 }
 
+export default Exchange
