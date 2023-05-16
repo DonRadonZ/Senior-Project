f62318a9215c5227a9e2dc8957b8d3862094608e
RadonZ <supachai27102000@gmail.com>
2023-05-16 12:23:01 +0700

    Change back exchange

--- frontend/src/Screen/Exchange/exchange.tsx
+++ frontend/src/Screen/Exchange/exchange.tsx
@@ -1,99 +1,24 @@
-import React, { useState } from "react";
-import {
-  TextInput,
-  View,
-  StyleSheet,
-  ImageBackground,
-  TouchableOpacity,
-} from "react-native";
-import BalanceCard from "../../Components/Card/BalanceCard";
-import { ScreenWidth } from "../../Components/shared";
-import { useNavigation } from "@react-navigation/native";
-import { colors } from "../../Components/Colors/colors";
-import { Text } from "@react-native-material/core";
+import React from 'react';
+import {ImageBackground} from 'react-native'; 
+// import MainContainer from '../../component/Container/MainContainer';
+import { LinearGradient } from 'expo-linear-gradient';
+import { colors } from '../../Components/Colors/colors';
+import SellButton from '../../Components/Button/SellButton';
+import BuyChoiceButton from '../../Components/Button/BuyChoiceButton';
+import SellChoiceButton from '../../Components/Button/SellChoiceButton';
+import styles from '../../Components/Container/Backgroundstyle';
 
-export const Exchange = () => {
-  const [balance, setBalance] = useState("");
-  const [senderNumber, setSenderNumber] = useState("");
-  const [accountNumber, setAccountNumber] = useState("");
-  const [amount, setAmount] = useState("");
-  const [description, setDescription] = useState("");
-
-  const navigation = useNavigation();
-
-  const goToConfirmationPage = () => {
-    navigation.navigate("ConfirmationPage", {
-      accountNumber,
-      amount,
-      description,
-    });
-  };
-
-  return (
+export const Exchange = () =>{
+  return(
     <ImageBackground
-      source={require("../../../assets/Background-image.jpg")}
-      style={styles.container}
-      resizeMode="cover"
-    >
-      <View style={styles.inputContainer}>
-        <BalanceCard />
-        <TextInput
-          style={styles.input}
-          placeholder="Account Number"
-          value={accountNumber}
-          onChangeText={setAccountNumber}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Amount"
-          value={amount}
-          onChangeText={setAmount}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Description"
-          value={description}
-          onChangeText={setDescription}
-        />
-      </View>
-      <TouchableOpacity
-        style={styles.confirmButton}
-        onPress={goToConfirmationPage}
-      >
-        <Text style={styles.confirmButtonText}>Continue</Text>
-      </TouchableOpacity>
-    </ImageBackground>
-  );
-};
+    source={require("../../../assets/Background-image.jpg")}
+    style={styles.container}
+    resizeMode="cover"
+  >
+          <BuyChoiceButton/>
+          <SellChoiceButton/>
+      </ImageBackground>
+      
+  )
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 25,
-    paddingTop: 40,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  inputContainer: {
-    marginBottom: 20,
-    width: ScreenWidth * 0.8,
-  },
-  input: {
-    height: 80,
-    backgroundColor: "white",
-    borderWidth: 2,
-    borderRadius: 15,
-    marginBottom: 40,
-    paddingHorizontal: 10,
-  },
-  confirmButton: {
-    backgroundColor: colors.orange,
-    paddingVertical: 24,
-    paddingHorizontal: 40,
-    borderRadius: 10,
-  },
-  confirmButtonText: {
-    color: colors.white,
-    fontSize: 18,
-  },
-});
