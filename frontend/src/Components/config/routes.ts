92a8b01bf96d419da6b373b3a901f0faf3cec11a
RadonZ <supachai27102000@gmail.com>
2023-05-17 01:12:28 +0700

    add route

---
+++ frontend/src/Components/config/routes.ts
@@ -0,0 +1,11 @@
+import   PaymentScreen  from "../../Screen/Payment/payment";
+import { IRouteProps } from "../library/RouteProp";
+
+const routes: IRouteProps[] = [
+    {
+        name: "Payment",
+        component: PaymentScreen
+    }
+]
+
+export default routes
