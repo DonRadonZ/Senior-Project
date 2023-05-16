f2d06c31cc52dc60cdb676fcf993c334e3373346
RadonZ <supachai27102000@gmail.com>
2023-05-16 12:47:02 +0700

    change background

--- frontend/src/Screen/Balance/balance.tsx
+++ frontend/src/Screen/Balance/balance.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import {Text, TouchableOpacity} from 'react-native'; 
+import {ImageBackground} from 'react-native'; 
 // import MainContainer from '../../component/Container/MainContainer';
 import { LinearGradient } from 'expo-linear-gradient';
 import { colors } from '../../Components/Colors/colors';
 
 import BalanceCard from '../../Components/Card/BalanceCard';
 
+import styles from '../../Components/Container/Backgroundstyle';
 export const Balance = () =>{
     return(
-        <LinearGradient
-            colors={[colors.malibu1,colors.malibu2]}
-            style={{
-                flex:1,
-                padding: 25,
-                paddingTop:40,
-                justifyContent: 'center',
-                alignItems:'center'
-            }}
-            >
+        <ImageBackground
+    source={require("../../../assets/Background-image.jpg")}
+    style={styles.container}
+    resizeMode="cover"
+  >
             <BalanceCard/>
-            </LinearGradient>
+            </ImageBackground>
         
     )
 }
--- frontend/src/Screen/Profile/profile.tsx
+++ frontend/src/Screen/Profile/profile.tsx
@@ -1,29 +1,26 @@
 import React from 'react';
-import {Text, TouchableOpacity} from 'react-native'; 
+import { ImageBackground,TouchableOpacity} from 'react-native'; 
 import { LinearGradient } from 'expo-linear-gradient';
 import { colors } from '../../Components/Colors/colors';
 import ProfileButton from '../../Components/Button/ProfileButton';
 // import MainContainer from '../../component/Container/MainContainer';
 
+import styles from '../../Components/Container/Backgroundstyle';
+
 export const Profile = () =>{
     return(
-        <LinearGradient
-        colors={[colors.malibu1,colors.malibu2]}
-        style={{
-            flex:1,
-            padding: 25,
-            paddingTop:40,
-            justifyContent: 'center',
-            alignItems:'center'
-        }}
-        >
-            <Text>Name: John Doe</Text>
+        <ImageBackground
+      source={require("../../../assets/Background-image.jpg")}
+      style={styles.container}
+      resizeMode="cover"
+    >
+           
             <ProfileButton onPress={()=>{}}>Add Account</ProfileButton>
             <ProfileButton onPress={()=>{}}>Notification</ProfileButton>
             <ProfileButton onPress={()=>{}}>Language Select</ProfileButton>
             <ProfileButton onPress={()=>{}}>Change Password</ProfileButton>
             
-        </LinearGradient>
+        </ImageBackground>
         
     )
 }
