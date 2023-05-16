2cbc08a222f356ea72a6c8d24df86b7a52d6cd1f
RadonZ <supachai27102000@gmail.com>
2023-05-16 12:25:31 +0700

    add background component

---
+++ frontend/src/Components/Container/Backgroundstyle.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 25,
+      paddingTop: 40,
+      justifyContent: "center",
+      alignItems: "center",
+    }
+  });
+  
+  export default styles
