68b8d8baf9b4edbcba33d066331626f88a1f3d45
RadonZ <supachai27102000@gmail.com>
2023-05-16 12:47:30 +0700

    change background

--- frontend/src/Screen/Home/home.tsx
+++ frontend/src/Screen/Home/home.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-//import {Text} from 'react-native'; 
+import {ImageBackground,StyleSheet} from 'react-native'; 
 import { Text } from "@react-native-material/core";
 import RegularText from '../../Components/Texts/RegularText';
 import { LinearGradient } from 'expo-linear-gradient';
 import { colors } from '../../Components/Colors/colors';
 // import MainContainer from '../../component/Container/MainContainer';
+import styles from '../../Components/Container/Backgroundstyle';
 
 export const Home = () =>{
     return(
-        <LinearGradient
-        colors={[colors.malibu1,colors.malibu2]}
-        style={{
-            flex:1,
-            justifyContent: 'center',
-            alignItems:'center'
-        }}
-        >
+        <ImageBackground
+      source={require("../../../assets/Background-image.jpg")}
+      style={styles.container}
+      resizeMode="cover"
+    >
                 <RegularText>Home</RegularText>
-            </LinearGradient>
+             </ImageBackground>
                 
             
         
