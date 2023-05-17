b3fda87c436ee3c2f782783cbabae69201016c8e
RadonZ <supachai27102000@gmail.com>
2023-05-18 01:09:53 +0700

    remove unused

--- frontend/src/Screen/Home/home.tsx
+++ frontend/src/Screen/Home/home.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import {ImageBackground,StyleSheet} from 'react-native'; 
 import { Text } from "@react-native-material/core";
 import RegularText from '../../Components/Texts/RegularText';
-import { LinearGradient } from 'expo-linear-gradient';
-import { colors } from '../../Components/Colors/colors';
 // import MainContainer from '../../component/Container/MainContainer';
 import styles from '../../Components/Container/Backgroundstyle';
 
