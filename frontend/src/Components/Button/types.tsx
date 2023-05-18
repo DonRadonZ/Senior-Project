9d6f672070f28c4b1cf3682b24f2f4970e039bd6
RadonZ <supachai27102000@gmail.com>
2023-05-18 17:39:03 +0700

    add prop button

--- frontend/src/Components/Button/types.tsx
+++ frontend/src/Components/Button/types.tsx
@@ -1,7 +1,21 @@
 import {ReactNode} from 'react';
 import {StyleProp,ViewStyle,TextStyle,GestureResponderEvent} from 'react-native';
 
-export interface ButtonProps{
+export interface BuyButtonProps{
+    children:ReactNode;
+    style?: StyleProp<ViewStyle>;
+    textStyle?: StyleProp<TextStyle>;
+    onPress: ((event: GestureResponderEvent) => void) | undefined
+}
+
+export interface SellButtonProps{
+    children:ReactNode;
+    style?: StyleProp<ViewStyle>;
+    textStyle?: StyleProp<TextStyle>;
+    onPress: ((event: GestureResponderEvent) => void) | undefined
+}
+
+export interface ProfileButtonProps{
     children:ReactNode;
     style?: StyleProp<ViewStyle>;
     textStyle?: StyleProp<TextStyle>;
