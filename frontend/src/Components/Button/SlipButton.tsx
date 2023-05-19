60e0614ab0e785d8dc145b79c47425a81773ea23
RadonZ <supachai27102000@gmail.com>
2023-05-19 14:56:42 +0700

    add new button

---
+++ frontend/src/Components/Button/SlipButton.tsx
@@ -0,0 +1,19 @@
+import React,{FunctionComponent} from "react";
+import { Text,Pressable } from "@react-native-material/core";
+import Buttonstyles from "./style";
+
+
+
+import { BuyButtonProps } from "./types";
+import { useNavigation } from "@react-navigation/core";
+
+
+const SlipButton:FunctionComponent<BuyButtonProps> = (props) => {
+  const navigation = useNavigation()
+  return(
+    <Pressable 
+    style={Buttonstyles.ButtonContainer} onPress={props.onPress}
+     ><Text style={Buttonstyles.ChoiceText}>{props.children}</Text></Pressable>
+  );
+}
+  export default SlipButton;
