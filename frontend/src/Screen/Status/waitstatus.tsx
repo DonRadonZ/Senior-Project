37c19b7053b0a1cd14871857571ccba341f83b93
RadonZ <supachai27102000@gmail.com>
2023-05-19 14:57:44 +0700

    add status

---
+++ frontend/src/Screen/Status/waitstatus.tsx
@@ -0,0 +1,52 @@
+import React,{FunctionComponent} from 'react';
+import {TouchableOpacity,ImageBackground} from 'react-native'; 
+// import MainContainer from '../../component/Container/MainContainer';
+import { Text } from '@react-native-material/core';
+
+import { LinearGradient } from 'expo-linear-gradient';
+import CheckButton from '../../Components/Button/CheckButton';
+
+import styles from '../Exchange/Buyer/style';
+import { StackNavigationProp, createStackNavigator } from '@react-navigation/stack';
+//import { IStackScreenProps } from '../../Components/library/StackScreenProps';
+
+import 'react-native-gesture-handler'
+
+//screen
+import { useNavigation } from '@react-navigation/native';
+import { RootStackParams } from '../../../App';
+
+
+
+
+const Stack = createStackNavigator()
+
+
+
+
+
+export const Wait = () => {
+  
+  const navigation = useNavigation<StackNavigationProp<RootStackParams>>()
+
+  
+
+  return(
+    <ImageBackground
+    source={require("../../../assets/Background-image.jpg")}
+    style={styles.container}
+    resizeMode="cover"
+    >
+    
+      <CheckButton 
+      onPress={()=>{
+        //go to payment
+        navigation.navigate("Payment")}}
+      >Examine</CheckButton>
+      
+      
+      </ImageBackground>
+    
+      
+  )
+}
