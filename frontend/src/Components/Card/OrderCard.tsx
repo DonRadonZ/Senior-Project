1beb2050f5b9342734956f4fd8f2c653d17acc39
RadonZ <supachai27102000@gmail.com>
2023-05-21 03:57:07 +0700

    update order card

--- frontend/src/Components/Card/OrderCard.tsx
+++ frontend/src/Components/Card/OrderCard.tsx
@@ -4,22 +4,28 @@ import { View } from "react-native";
 import { Box,Text } from "@react-native-material/core";
 import { colors } from "../Colors/colors";
 import BalanceView from "./Balancestyles";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParams } from "../../../App";
 
-const OrderCardA = () => (
-  <Box style={BalanceView.orderContainer}>
-    <View style={{}}>
-      <Text
-        style={BalanceView.BalanceText}
-      >
-        Order1
-      </Text> 
-    <Text style={BalanceView.TimeStampText}>15/5/2023</Text>
-    </View>
-    <TouchableOpacity style={{
-      alignItems: "center"
-    }}><Text style={BalanceView.OrderViewText}>View</Text></TouchableOpacity>
-  </Box>
-);
 
+const OrderCardA = () => {
+  const navigation = useNavigation<StackNavigationProp<RootStackParams>>()
+  return (
+    <Box style={BalanceView.orderContainer}>
+      <View style={{}}>
+        <Text
+          style={BalanceView.BalanceText}
+        >
+          Order1
+        </Text>
+        <Text style={BalanceView.TimeStampText}>15/5/2023</Text>
+      </View>
+      <TouchableOpacity style={{
+        alignItems: "center"
+      }} onPress={()=>{navigation.navigate('EndUserFile')}}><Text style={BalanceView.OrderViewText}>View</Text></TouchableOpacity>
+    </Box>
+  );
+}
 export default OrderCardA;
 
