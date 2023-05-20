43fbde478dc6c8c3214c07ea01852c1d0164c61d
RadonZ <supachai27102000@gmail.com>
2023-05-20 19:01:55 +0700

    add notification

---
+++ frontend/src/Screen/Notification/notification.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {ImageBackground,StyleSheet,SafeAreaView} from 'react-native'; 
+import { Text } from "@react-native-material/core";
+import RegularText from '../../Components/Texts/RegularText';
+// import MainContainer from '../../component/Container/MainContainer';
+//import styles from '../../Components/Container/Backgroundstyle';
+import ServiceLists from "../../Components/List/ServiceList"
+import services from "../../mock/services.json"
+import { useNavigation } from '@react-navigation/native';
+import { Service } from '../../../App';
+export const Home = () => {
+  
+    return(
+        <ImageBackground
+        source={require("../../../assets/Background-image.jpg")}
+        style={{flex:1}}
+        resizeMode="cover"
+      >
+        <SafeAreaView>
+          <RegularText>Friend List</RegularText>
+
+          <RegularText>Service</RegularText>
+          <ServiceLists onPress={()=>{}} list={services}/>
+        </SafeAreaView>
+                
+             </ImageBackground>
+                
+            
+        
+    )
+}
