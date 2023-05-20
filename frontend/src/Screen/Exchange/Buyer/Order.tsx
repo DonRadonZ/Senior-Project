a5d516d67d9da6383b9a3b5ef534398e501d8941
RadonZ <supachai27102000@gmail.com>
2023-05-20 22:11:53 +0700

    add addnew button

--- frontend/src/Screen/Exchange/Buyer/Order.tsx
+++ frontend/src/Screen/Exchange/Buyer/Order.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import {ImageBackground} from 'react-native'; 
 // import MainContainer from '../../component/Container/MainContainer';
+import AddNewButton from '../../../Components/Button/AddNewButton';
 
 
 import OrderCardA from '../../../Components/Card/OrderCard';
 import OrderCardB from '../../../Components/Card/OrderCard';
 
-import styles from '../../../Components/Container/Backgroundstyle';
-export const BuyOrder = () =>{
+import styles from './style';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParams } from '../../../../App';
+
+const BuyList = () => {
+    
+    const navigation = useNavigation<StackNavigationProp<RootStackParams>>();
     return(
         <ImageBackground
     source={require("../../../../assets/Background-image.jpg")}
@@ -16,8 +23,10 @@ export const BuyOrder = () =>{
         >
             
             <OrderCardA />
-            <OrderCardB/>
+            <OrderCardB />
+            <AddNewButton onPress={() => {navigation.navigate("Payment")}} children={undefined} />
             </ImageBackground>
         
     )
 }
+export default BuyList
