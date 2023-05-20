3473f99a7779361e12bb0bd3a0ad1076b46f2c6c
RadonZ <supachai27102000@gmail.com>
2023-05-21 03:58:45 +0700

    add get buyer slip

---
+++ frontend/src/Screen/Exchange/EndUser/getbuyerslip.tsx
@@ -0,0 +1,53 @@
+import React,{FunctionComponent} from 'react';
+import {TouchableOpacity,ImageBackground} from 'react-native'; 
+// import MainContainer from '../../component/Container/MainContainer';
+import { Text } from '@react-native-material/core';
+import { colors } from '../../../Components/Colors/colors';
+import { LinearGradient } from 'expo-linear-gradient';
+import BuyChoiceButton from '../../../Components/Button/BuyChoiceButton';
+import SellChoiceButton from '../../../Components/Button/SellChoiceButton';
+
+import styles from '../../../Components/Container/Backgroundstyle';
+import { StackNavigationProp, createStackNavigator } from '@react-navigation/stack';
+//import { IStackScreenProps } from '../../Components/library/StackScreenProps';
+
+import 'react-native-gesture-handler'
+
+//screen
+import { useNavigation } from '@react-navigation/native';
+import { RootStackParams } from '../../../../App';
+
+
+
+
+const Stack = createStackNavigator()
+
+
+
+
+
+export const EndReceive = () => {
+  
+  const navigation = useNavigation<StackNavigationProp<RootStackParams>>()
+
+  
+
+  return(
+    <ImageBackground
+    source={require("../../../../assets/Background-image.jpg")}
+    style={styles.container}
+    resizeMode="cover"
+    >
+    
+      <BuyChoiceButton 
+      onPress={()=>{
+        //go to payment
+        navigation.navigate("Payment")}}
+      >Press for Buyer</BuyChoiceButton>
+      <SellChoiceButton onPress={()=>{alert("List")}}>Press for End User</SellChoiceButton>
+      
+      </ImageBackground>
+    
+      
+  )
+}
