dd6ed072203183c4b0f0a81254f2c00c425431d0
RadonZ <supachai27102000@gmail.com>
2023-05-21 03:59:01 +0700

    future use

---
+++ frontend/src/Screen/Register/successstatus.tsx
@@ -0,0 +1,57 @@
+import React,{FunctionComponent} from 'react';
+import {TouchableOpacity,ImageBackground} from 'react-native'; 
+// import MainContainer from '../../component/Container/MainContainer';
+import { Text } from '@react-native-material/core';
+
+import { LinearGradient } from 'expo-linear-gradient';
+import CheckButton from '../../Components/Button/CheckButton';
+
+import styles from '../Exchange/Buyer/style';
+import { StackNavigationProp, createStackNavigator } from '@react-navigation/stack';
+//import { IStackScreenProps } from '../../Components/library/StackScreenProps';
+
+import 'react-native-gesture-handler'
+
+//screen
+import { useNavigation } from '@react-navigation/native';
+import { RootStackParams } from '../../../App';
+
+//icons
+import { FontAwesome } from '@expo/vector-icons';
+import { colors } from '../../Components/Colors/colors';
+
+
+
+
+const Stack = createStackNavigator()
+
+
+
+
+
+ const RegisterSuccess = () => {
+  
+  const navigation = useNavigation<StackNavigationProp<RootStackParams>>()
+
+  
+
+  return(
+    <ImageBackground
+    source={require("../../../assets/Background-image.jpg")}
+    style={styles.container}
+    resizeMode="cover"
+    >
+          <FontAwesome name="check-circle" size={200} color={colors.navyblue} style={{marginBottom:40}} />
+      <CheckButton 
+      onPress={()=>{
+        //go to payment
+        navigation.navigate("MyLogin")}}
+      >Go to Login Page</CheckButton>
+      
+      
+      </ImageBackground>
+    
+      
+  )
+}
+export default RegisterSuccess
