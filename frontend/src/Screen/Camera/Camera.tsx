342f4a6e48c13d1fdf1c2323ba0badd0a484c9e0
RadonZ <supachai27102000@gmail.com>
2023-05-21 03:56:53 +0700

    test camera

--- frontend/src/Screen/Camera/Camera.tsx
+++ frontend/src/Screen/Camera/Camera.tsx
@@ -1,10 +1,31 @@
 import { Camera, CameraType } from 'expo-camera';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { colors } from '../../Components/Colors/colors';
+import * as ImagePicker from 'expo-image-picker';
+import * as MediaLibrary from 'expo-media-library'
 
-export default function UseCamera() {
+
+export default function UseCamera(this: any) {
+  const [image, setImage] = useState(null);
+  const [camera, setCamera] = useState(null);
   const [type, setType] = useState(CameraType.back);
+  const [isCameraReady, setIsCameraReady] = useState(false);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [preview, setIsPreview] = useState(false);
+
+  const cameraRef = useRef(null);
+
+
+
+
+  const onCameraReady = () => {
+  setIsCameraReady(true);
+  };
+
+
+
 
   if (!permission) {
     // Camera permissions are still loading
@@ -25,18 +46,33 @@ export default function UseCamera() {
     setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
   }
 
+  // const snap = async () => {
+  //   if (cameraRef) {
+  //     const data = await cameraRef.current.takePictureAsync();
+  //     console.log(data);
+  //   }
+    
+  // }
+
+
   return (
     <View style={styles.container}>
       <Camera style={styles.camera} type={type}>
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
-            <Text style={styles.text}>Flip Camera</Text>
+            <Ionicons name="camera-reverse" size={24} color="black" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} disabled={!isCameraReady} onPress={()=>{}}
+          >
+            <Ionicons name="camera" size={24} color="black" />
           </TouchableOpacity>
         </View>
       </Camera>
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
