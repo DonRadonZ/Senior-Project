930b09ddd98400e1114e7a48ef05d108ed067f3b
RadonZ <supachai27102000@gmail.com>
2023-05-20 22:10:52 +0700

    add order card

--- frontend/src/Components/Card/OrderCard.tsx
+++ frontend/src/Components/Card/OrderCard.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
+import { View } from "react-native";
 import { Box,Text } from "@react-native-material/core";
 import { colors } from "../Colors/colors";
 import BalanceView from "./Balancestyles";
 
 const OrderCardA = () => (
-  <Box style={BalanceView.balanceContainer}>
-    <Text style={BalanceView.BalanceText}>Order1</Text>
-        <TouchableOpacity><Text style={BalanceView.OrderViewText}>View</Text></TouchableOpacity>
+  <Box style={BalanceView.orderContainer}>
+    <View style={{}}>
+      <Text
+        style={BalanceView.BalanceText}
+      >
+        Order1
+      </Text> 
     <Text style={BalanceView.TimeStampText}>15/5/2023</Text>
+    </View>
+    <TouchableOpacity style={{
+      alignItems: "center"
+    }}><Text style={BalanceView.OrderViewText}>View</Text></TouchableOpacity>
   </Box>
 );
 
