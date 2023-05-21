92744e4dc2d32ee6c74d343e946ad4ace071a5ba
RadonZ <supachai27102000@gmail.com>
2023-05-22 03:49:37 +0700

    update home and text

--- frontend/src/Components/Texts/HomeText.tsx
+++ frontend/src/Components/Texts/HomeText.tsx
@@ -8,8 +8,8 @@ import { colors } from "../Colors/colors";
 
 import {TextProps} from "./types";
 
-const RegularText: FunctionComponent<TextProps> = (props) => {
-    return <Text style={{fontSize:25,marginBottom:15,marginRight:180}}>{props.children}</Text>;
+const HomeText: FunctionComponent<TextProps> = (props) => {
+    return <Text style={props.textStyles}>{props.children}</Text>;
 }
 
-export default RegularText
+export default HomeText
