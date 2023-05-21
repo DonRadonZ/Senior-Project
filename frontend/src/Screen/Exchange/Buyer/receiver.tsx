1f1a1ac71a9b93b2bf42fe5f12af49a7006a1d92
RadonZ <supachai27102000@gmail.com>
2023-05-22 03:50:41 +0700

    add component

--- frontend/src/Screen/Exchange/Buyer/receiver.tsx
+++ frontend/src/Screen/Exchange/Buyer/receiver.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Text, View, StyleSheet, ImageBackground } from "react-native";
-import { Box } from "@react-native-material/core";
+import { Button,Box } from "@react-native-material/core";
 import { colors } from "../../../Components/Colors/colors";
 import SellButton from "../../../Components/Button/SellButton";
 import styles from "./style";
 import BuyChoiceButton from "../../../Components/Button/BuyChoiceButton";
+import {  StackNavigationProp} from "@react-navigation/stack";
+import { RootStackParams } from "../../../../App";
+import { useNavigation } from "@react-navigation/native";
+import CancelButton from "../../../Components/Button/CancelButton";
+import ExamineButton from "../../../Components/Button/ExamineButton";
 interface ConfirmationPageProps {
   accountNumber: string;
   amount: string;
@@ -15,21 +20,33 @@ const ReceiverPage = ({
   // accountNumber,
   // amount,
   // description,
+  
 }) => {
+  const navigation = useNavigation<StackNavigationProp<RootStackParams>>();
   return (
+
     <ImageBackground
     source={require("../../../../assets/Background-image.jpg")}
     style={styles.container}
     resizeMode="cover"
     >
-    <Box style={styles.slipcontainer}>
+      
+      <Box style={styles.slipcontainer}>  
+        <View>
+      <Text style={styles.mainText}>Information</Text>
       <Text style={styles.confirmationText}>
-        Receiver Code: 
+        Receiver Code: 2583112578
       </Text>
       <Text style={styles.confirmationText}>Date: 1231546</Text>
-      <Text style={styles.confirmationText}>Time: </Text>
+          <Text style={styles.confirmationText}>Time: 12:30:00 </Text>
+        </View>
+        <View>
+          <Button title="Slip" color="black" onPress={()=>{navigation.navigate("MySlip")}}/>
+        </View>
       </Box>
-     <BuyChoiceButton onPress={undefined}>Continue</BuyChoiceButton> 
+      {/* <Button title="Pending" loading disabled /> */}
+      <ExamineButton onPress={()=>{navigation.navigate("ReceiverSlip")}} >Next</ExamineButton> 
+      {/* <CancelButton onPress={()=>{}}>cancel send</CancelButton> */}
     </ImageBackground>
   );
 };
