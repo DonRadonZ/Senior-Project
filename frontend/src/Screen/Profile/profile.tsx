dc58d0c6f4caf42923d6370ccd8890afb590459e
RadonZ <supachai27102000@gmail.com>
2023-05-22 03:52:13 +0700

    add profile

--- frontend/src/Screen/Profile/profile.tsx
+++ frontend/src/Screen/Profile/profile.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import { ImageBackground,TouchableOpacity} from 'react-native'; 
+import { ImageBackground,TouchableOpacity, View,Text} from 'react-native'; 
 import { LinearGradient } from 'expo-linear-gradient';
 import { colors } from '../../Components/Colors/colors';
 import ProfileButton from '../../Components/Button/ProfileButton';
+import { Avatar } from '@react-native-material/core';
 // import MainContainer from '../../component/Container/MainContainer';
 
 import styles from '../../Components/Container/Backgroundstyle';
+import { ScreenWidth } from '../../Components/shared';
 
 export const Profile = () =>{
     return(
         <ImageBackground
       source={require("../../../assets/Background-image.jpg")}
-      style={styles.container}
+      style={styles.profilecontainer}
       resizeMode="cover"
-    >
+        >
+            <Avatar label="Pimlapat Nakwijit" size={75} />
+            <View
+                style={{
+                    left: 100,
+                    bottom:50
+                }} 
+            >
+            
+                
+                <Text style={{ fontSize: 16 }}>Name: Pimlapat Nakwijit
+                    
+                </Text>
+                 <Text style={{ fontSize: 16 }}> ID:708503922</Text>
+
+            </View>
+
            
             <ProfileButton onPress={()=>{}}>Add Account</ProfileButton>
             <ProfileButton onPress={()=>{}}>Notification</ProfileButton>
