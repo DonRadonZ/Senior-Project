458b9aa30a84c4f5d8a9aef4b8343069c8393d60
RadonZ <supachai27102000@gmail.com>
2023-05-22 03:51:52 +0700

    add sent pic

--- frontend/src/Screen/Exchange/EndUser/SentPic.tsx
+++ frontend/src/Screen/Exchange/EndUser/SentPic.tsx
@@ -1,65 +1,121 @@
-import { Camera, CameraType } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import {
+    Camera,
+    CameraType,
+    type CameraPictureOptions,
+    requestPermissionsAsync,
+} from 'expo-camera';
+import { AntDesign,Entypo,Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { MediaTypeOptions, launchImageLibraryAsync } from 'expo-image-picker';
+import { colors } from '../../../Components/Colors/colors';
+import { ScreenWidth } from '../../../Components/shared';
 
-export default function EndUserfile() {
-  const [type, setType] = useState(CameraType.back);
-  const [permission, requestPermission] = Camera.useCameraPermissions();
+export const EndUserFile = () => {
+    const cameraRef = useRef<Camera>(null);
+    const { navigate } = useNavigation();
+    const [cameraType, setCameraType] = useState(CameraType.back);
+    const [permission, requestPermission] = Camera.useCameraPermissions();
+    const [cameraReady, setCameraReady] = useState(false);
 
-  if (!permission) {
-    // Camera permissions are still loading
-    return <View />;
-  }
+    const cameraNotReady = !cameraReady || !cameraRef.current;
 
-  if (!permission.granted) {
-    // Camera permissions are not granted yet
+    const snap = async () => {
+        if (cameraNotReady) {
+            return;
+        }
+        const options: CameraPictureOptions = {
+            quality: 0.5,
+            skipProcessing: true,
+
+        };
+        const result = cameraRef.current.takePictureAsync(options);
+        //navigate("CheckPhoto", params);
+    }
+
+    const ImageGallery = async () => {
+        const { assets } = await launchImageLibraryAsync({
+            mediaTypes: MediaTypeOptions.All,
+            allowsMultipleSelection: false,
+            
+        });
+        if (!assets || !(assets.length > 0)) {
+            return;
+        }
+         const params: { image?: string; images?: string[]; video?: string } = {};
+        if (assets.length === 1) {
+            const field = assets[0].type?.startsWith("video") ? "video" : "image";
+           params[field] = assets[0].uri;
+             } else if (assets.length > 1) {
+               params.images = assets.map((asset) => asset.uri);
+             }
+             //navigate("CreatePost", params);
+         };
+             
+    
+
+    if (!permission?.granted) {
+        requestPermission();
+        console.log(permission?.granted);
+    }
+
+    const toggleCameraType = () => {
+        setCameraType((current) =>
+            current === CameraType.back ? CameraType.front : CameraType.back
+        );
+    };
+
     return (
-      <View style={styles.container}>
-        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-        <Button onPress={requestPermission} title="grant permission" />
-      </View>
-    );
-  }
-
-  function toggleCameraType() {
-    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
-  }
-
-  return (
-    <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
-            <Text style={styles.text}>Flip Camera</Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
-    </View>
-  );
+        <View style={{ flex: 1, backgroundColor: colors.black }}>
+            <Camera
+                ref={cameraRef}
+                style={{ flex: 1 }}
+                type={cameraType}
+                ratio="4:3"
+                onCameraReady={() => {
+                    setCameraReady(true);
+                }}
+            />
+            <View
+                style={{
+                    position: 'absolute',
+                    flexDirection: 'row',
+                    justifyContent: 'flex-start',
+                    width: ScreenWidth,
+                    alignItems: 'flex-start',
+            }}
+            >
+            <AntDesign name="close" size={30} color={colors.white} />
+            </View>
+            <View
+                style={{
+                    position: 'absolute',
+                    flexDirection: 'row',
+                    justifyContent: 'space-around',
+                    width: ScreenWidth,
+                    bottom: 12,
+                    alignItems: 'center',
+            }}    
+            >
+                <TouchableOpacity onPress={ImageGallery}>
+                    <Entypo name="image" size={30} color={colors.white} />
+                </TouchableOpacity>
+                {cameraReady && (
+                    <TouchableOpacity
+                        onPress={snap}
+                        style={{
+                            backgroundColor: colors.white,
+                            width: 60,
+                            height: 60,
+                            borderRadius:30
+                            }}
+                    />
+                )}
+                <TouchableOpacity onPress={toggleCameraType}>
+                    <Ionicons name='ios-camera-reverse' size={30} color={colors.white} />
+                </TouchableOpacity>
+            </View>
+         </View>
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  camera: {
-    flex: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    margin: 64,
-  },
-  button: {
-    flex: 1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-});
