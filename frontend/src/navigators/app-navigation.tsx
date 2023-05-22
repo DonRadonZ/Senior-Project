c373c53b761faf71e5ba824c5eb3a51ef1ee92af
RadonZ <supachai27102000@gmail.com>
2023-05-22 22:37:49 +0700

    add navigator

--- frontend/src/navigators/app-navigation.tsx
+++ frontend/src/navigators/app-navigation.tsx
@@ -2,8 +2,32 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ActivityIndicator, View } from "react-native";
 
 //authscreen
+import { LoginScreen } from "../Screen/Login/login";
+import { RegisterScreen } from "../Screen/Register/register";
+import { RegisterMailScreen } from "../Screen/Register/registerMail";
+import { RegisterIdScreen } from "../Screen/Register/registerId";
+import { RegisterScanFaceScreen } from "../Screen/Register/registerScanFace";
+import { RegisterVerifiedScreen } from "../Screen/Register/registerVerified";
+
+//settingscreen
 
 
 //navigation
 import { RootStackParams } from "./navigator-type";
 import { BottomTab } from "./bottom-tab";
+
+//payment
+
+
+//API
+
+
+//ExchangeScreen
+
+
+const RootStack = createNativeStackNavigator<RootStackParams>();
+
+
+export const AppNavigator = () => {
+    
+}
