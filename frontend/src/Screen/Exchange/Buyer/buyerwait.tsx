f71452a7b5ba51635ad1e3f160ea4e22b9f71c29
RadonZ <supachai27102000@gmail.com>
2023-05-23 01:56:35 +0700

    add buyer wallet

---
+++ frontend/src/Screen/Exchange/Buyer/buyerwait.tsx
@@ -0,0 +1,56 @@
+import React,{FunctionComponent} from 'react';
+import {TouchableOpacity,ImageBackground} from 'react-native'; 
+
+
+
+
+
+import { StackNavigationProp, createStackNavigator } from '@react-navigation/stack';
+
+
+import 'react-native-gesture-handler'
+
+//screen
+import { useNavigation } from '@react-navigation/native';
+
+
+//icons
+import { AntDesign } from '@expo/vector-icons';
+import { RootStackParams } from '../../../../App';
+import styles from './style';
+import CheckButton from '../../../Components/Button/CheckButton';
+
+
+
+
+const Stack = createStackNavigator()
+
+
+
+
+
+export const Wait = () => {
+  
+  const navigation = useNavigation<StackNavigationProp<RootStackParams>>()
+
+  
+
+  return(
+    <ImageBackground
+    source={require("../../../assets/Background-image.jpg")}
+    style={styles.container}
+    resizeMode="cover"
+    >
+      <AntDesign name="exclamationcircleo" size={108} color="black" style={{marginBottom:75}} />
+      <CheckButton 
+      onPress={()=>{
+        //go to payment
+        navigation.navigate("Payment")}}
+      >Examine</CheckButton>
+      
+      
+      </ImageBackground>
+    
+      
+  )
+}
