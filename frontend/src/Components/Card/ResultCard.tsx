674b618b99b075eeac9a93a8a00369a4674aee14
RadonZ <supachai27102000@gmail.com>
2023-05-22 19:54:30 +0700

    update result card

--- frontend/src/Components/Card/ResultCard.tsx
+++ frontend/src/Components/Card/ResultCard.tsx
@@ -5,9 +5,10 @@ import BalanceView from "./Balancestyles";
 
 const ResultCard = () => (
   <Box style={BalanceView.ResultContainer}>
-    <Text style={BalanceView.IDText}>Account ID: </Text>
-    <Text style={BalanceView.DateText}>Date:</Text>
-    <Text style={BalanceView.TimeText}>Time:</Text>
+    <Text style={BalanceView.IDText}>Success</Text>
+    <Text style={BalanceView.IDText}>Account ID: 2583112578</Text>
+    <Text style={BalanceView.DateText}>Date:15/5/2023</Text>
+    <Text style={BalanceView.TimeText}>Time:12:00:00</Text>
   </Box>
 );
 
