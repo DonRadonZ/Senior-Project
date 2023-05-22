5fba0e6527b2a3ba475ac0fc08bd8ab450131ab9
RadonZ <supachai27102000@gmail.com>
2023-05-22 19:54:49 +0700

    add navigator

---
+++ frontend/src/navigators/app-navigation.tsx
@@ -0,0 +1,9 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ActivityIndicator, View } from "react-native";
+
+//authscreen
+
+
+//navigation
+import { RootStackParams } from "./navigator-type";
+import { BottomTab } from "./bottom-tab";
---
+++ frontend/src/navigators/navigator-type.ts
@@ -0,0 +1,44 @@
+
+import {
+    CompositeScreenProps,
+    NavigatorScreenParams
+} from "@react-navigation/native"
+import type { NativeStackScreenProps } from "@react-navigation/native-stack"
+import type { BottomTabScreenProps } from "@react-navigation/bottom-tabs"
+
+export type RootStackParams = {
+    Tab: NavigatorScreenParams<BottomTabTypes>
+}
+
+export type RootStackScreenProps<T extends keyof RootStackParams> =
+    NativeStackScreenProps<RootStackParams, T>;
+
+export type BottomTabTypes = {
+    Home: undefined,
+    Balance: undefined,
+    QRScan: undefined,
+    Exchange: undefined,
+    Profile: undefined,
+}
+
+export type AuthStackType = {
+    SignIn: undefined,
+    Signup: undefined,
+    ConfirmEmail: { email?: string },
+    ForgotPassword: undefined,
+    NewPassword: undefined,
+
+}
+
+export type typeBottomTabNavigationProps<T extends keyof BottomTabTypes> = 
+    CompositeScreenProps<
+        BottomTabScreenProps<BottomTabTypes, T>,
+        RootStackScreenProps<keyof RootStackParams>
+    >;
+
+type AllStackParamList = RootStackParams & BottomTabTypes;
+declare global {
+    namespace ReactNavigation{
+        interface RootStackParams extends AllStackParamList {}
+    }
+}
