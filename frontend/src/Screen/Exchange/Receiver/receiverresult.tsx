3fa580d9d8a14f77bcdb73362b9ea8a7ade6b675
RadonZ <supachai27102000@gmail.com>
2023-05-23 16:50:07 +0700

    add result styles

--- frontend/src/Screen/Exchange/Receiver/receiverresult.tsx
+++ frontend/src/Screen/Exchange/Receiver/receiverresult.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, StyleSheet,ImageBackground } from "react-native";
+import { Text, View, StyleSheet,ImageBackground, Button } from "react-native";
 import { colors } from "../../../Components/Colors/colors";
 import ReturnButton from "../../../Components/Button/ReturnButton";
 import { AntDesign, MaterialCommunityIcons } from '@expo/vector-icons';
@@ -24,17 +24,22 @@ export const ReceiverResultScreen = ({
   return (
     <ImageBackground
     source={require("../../../../assets/Background-image.jpg")}
-    style={styles.container}
+    style={styles.resultcontainer}
     resizeMode="cover"
       >
-          <AntDesign name="checkcircleo" size={72} color= {colors.orange} style={{marginTop:100}} />
-    
+        <View style ={{top:-80,alignItems:'center'}}>
+          <AntDesign name="checkcircleo" size={80} color= {colors.maingreen} style={{marginTop:100}} />
+          <Text style={{marginTop:20,marginBottom:-40,fontSize:30,color:colors.maingreen}}>Success</Text>
+        </View>
       {/* <Text style={styles.confirmationText}>
         Account ID: 
       </Text>
       <Text style={styles.confirmationText}>Date:</Text>
         <Text style={styles.confirmationText}>Time: </Text> */}
-        
+        <View style={{left:70,flexDirection:"row"}}>
+        <TouchableOpacity style={{backgroundColor:colors.malibu2,marginRight:20,width:70,height:30,alignItems:'center',justifyContent:"center",borderRadius:5}}><Text>Evidence</Text></TouchableOpacity>
+          <TouchableOpacity style={{backgroundColor:colors.malibu1,width:70,height:30,alignItems:'center',justifyContent:"center",borderRadius:5}}><Text>Slip</Text></TouchableOpacity>
+        </View>
       <ResultCard/>
       
       <ReturnButton children={undefined} onPress={()=>{navigation.navigate(('EndUser'))}}/>
@@ -48,6 +53,12 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 25,
     paddingTop: 40,
+    //justifyContent: "space-between",
+    alignItems: "center",
+  },resultcontainer: {
+    flex: 1,
+    padding: 25,
+    //paddingTop: 40,
     //justifyContent: "space-between",
     alignItems: "center",
   },
