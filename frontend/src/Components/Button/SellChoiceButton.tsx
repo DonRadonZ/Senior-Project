e645e261c17885d02fca75ca8215f4656ea6d714
RadonZ <supachai27102000@gmail.com>
2023-05-23 14:19:46 +0700

    change to touchableopacity

--- frontend/src/Components/Button/SellChoiceButton.tsx
+++ frontend/src/Components/Button/SellChoiceButton.tsx
@@ -1,5 +1,6 @@
 import React,{FunctionComponent} from "react";
 import { Text,Pressable } from "@react-native-material/core";
+import { TouchableOpacity } from "react-native/";
 import Buttonstyles from "./style";
 
 
@@ -11,9 +12,9 @@ import { useNavigation } from "@react-navigation/core";
 const SellChoiceButton:FunctionComponent<SellButtonProps> = (props) => {
   const navigation = useNavigation()
   return(
-    <Pressable 
+    <TouchableOpacity
     style={Buttonstyles.ButtonContainer} onPress={props.onPress}
-     ><Text style={Buttonstyles.ChoiceText}>{props.children}</Text></Pressable>
+     ><Text style={Buttonstyles.ChoiceText}>{props.children}</Text></TouchableOpacity>
   );
 }
 export default SellChoiceButton;
