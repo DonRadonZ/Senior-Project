6868530c04656142d89200341c6c307b72882bbe
RadonZ <supachai27102000@gmail.com>
2023-05-23 16:48:32 +0700

    adjust buyer status

--- frontend/src/Screen/Exchange/Buyer/buyerstatus.tsx
+++ frontend/src/Screen/Exchange/Buyer/buyerstatus.tsx
@@ -30,19 +30,21 @@ export const BuyerStatusScreen = ({
     style={styles.container}
     resizeMode="cover"
     >
-      
+      <View style={{alignItems:'flex-end'}}>
+          <Button title="Slip" color="black" onPress={()=>{navigation.navigate("MySlip")}}/>
+        </View>
       <Box style={styles.slipcontainer}>  
-        <View>
+        <View >
+          <View style={{alignItems:'center',marginLeft:135}}>
       <Text style={styles.mainText}>Information</Text>
+      </View>
       <Text style={styles.confirmationText}>
         Receiver Code: 2583112578
       </Text>
       <Text style={styles.confirmationText}>Date: 1231546</Text>
           <Text style={styles.confirmationText}>Time: 12:30:00 </Text>
         </View>
-        <View>
-          <Button title="Slip" color="black" onPress={()=>{navigation.navigate("MySlip")}}/>
-        </View>
+        
       </Box>
       {/* <Button title="Pending" loading disabled /> */}
       <ExamineButton onPress={()=>{navigation.navigate("ReceiverSlip")}} >Next</ExamineButton> 
