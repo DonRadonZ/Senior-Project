bae77d72a2b02ca9c893335277652ee63ddac396
RadonZ <supachai27102000@gmail.com>
2023-05-23 16:46:33 +0700

    add result card

--- frontend/src/Components/Card/ResultCard.tsx
+++ frontend/src/Components/Card/ResultCard.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import { Box,Text } from "@react-native-material/core";
 import { colors } from "../Colors/colors";
 import BalanceView from "./Balancestyles";
+import { View } from "react-native";
 
 const ResultCard = () => (
   <Box style={BalanceView.ResultContainer}>
-    <Text style={BalanceView.IDText}>Success</Text>
+    <View style={{marginLeft:100}}>
+    <Text style={BalanceView.HeaderText}>informaton</Text>
+    </View>
     <Text style={BalanceView.IDText}>Account ID: 2583112578</Text>
     <Text style={BalanceView.DateText}>Date:15/5/2023</Text>
     <Text style={BalanceView.TimeText}>Time:12:00:00</Text>
