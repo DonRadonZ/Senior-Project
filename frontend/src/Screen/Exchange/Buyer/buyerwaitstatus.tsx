eb5e01a367ff8fcdb44a84458ef2fcc3ae73fe96
RadonZ <supachai27102000@gmail.com>
2023-05-23 16:49:01 +0700

    add buyer wait

---
+++ frontend/src/Screen/Exchange/Buyer/buyerwaitstatus.tsx
@@ -0,0 +1,55 @@
+import React,{FunctionComponent} from 'react';
+import {TouchableOpacity,ImageBackground} from 'react-native'; 
+// import MainContainer from '../../component/Container/MainContainer';
+import { Text } from '@react-native-material/core';
+
+import { LinearGradient } from 'expo-linear-gradient';
+import CheckButton from '../../../Components/Button/CheckButton';
+
+import styles from '../../Exchange/Buyer/style';
+import { StackNavigationProp, createStackNavigator } from '@react-navigation/stack';
+//import { IStackScreenProps } from '../../Components/library/StackScreenProps';
+
+import 'react-native-gesture-handler'
+
+//screen
+import { useNavigation } from '@react-navigation/native';
+import { RootStackParams } from '../../../../App';
+
+//icons
+import { AntDesign } from '@expo/vector-icons';
+
+
+
+
+const Stack = createStackNavigator()
+
+
+
+
+
+export const Wait = () => {
+  
+  const navigation = useNavigation<StackNavigationProp<RootStackParams>>()
+
+  
+
+  return(
+    <ImageBackground
+    source={require("../../../assets/Background-image.jpg")}
+    style={styles.container}
+    resizeMode="cover"
+    >
+      <AntDesign name="exclamationcircleo" size={108} color="black" style={{marginBottom:75}} />
+      <CheckButton 
+      onPress={()=>{
+        //go to payment
+        navigation.navigate("Payment")}}
+      >Examine</CheckButton>
+      
+      
+      </ImageBackground>
+    
+      
+  )
+}
