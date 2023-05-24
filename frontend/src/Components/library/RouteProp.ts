ec53877eb27f4e6010fe8a44ede113848ece1a71
RadonZ <supachai27102000@gmail.com>
2023-05-24 18:41:43 +0700

    remove unused

--- frontend/src/Components/library/RouteProp.ts
+++
@@ -1,8 +0,0 @@
-import  React  from 'react';
-import { IStackScreenProps } from "./StackScreenProps";
-
-
-export interface IRouteProps {
-    component: React.FunctionComponent<IStackScreenProps>;
-    name: string;
-}
