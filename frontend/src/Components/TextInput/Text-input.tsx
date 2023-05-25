5be53e2c47f16d28a29590f60569bd14658dad30
RadonZ <supachai27102000@gmail.com>
2023-05-25 15:44:36 +0700

    add text inputs

--- frontend/src/Components/TextInput/Text-input.tsx
+++ frontend/src/Components/TextInput/Text-input.tsx
@@ -3,10 +3,36 @@ import { View,StyleSheet,Text } from "react-native";
 import { TextInput } from "react-native-paper";
 import { colors } from "../Colors/colors";
 
+
 type Props = ComponentProps<typeof TextInput>&{errorText?: string};
 
 const TextInputs = ({errorText, ...props}: Props) =>(
- <View>
-    
+ <View style={styles.container}>
+    <TextInput
+      style={styles.input}
+      selectionColor={colors.black}
+      underlineColor="transparent"
+      mode="outlined"
+      {...props}
+      />
+      {errorText ? <Text style={styles.error}>{errorText}</Text>:null}
  </View>
-)
+);
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        marginVertical: 12,
+    },
+    input:{
+        backgroundColor: colors.black,
+    },
+    error: {
+        fontSize: 14,
+        color: colors.red,
+        paddingHorizontal: 4,
+        paddingTop:4,
+    }
+})
+
+export default memo(TextInputs);
