bdd46d93000985834e8814492c9a575549e549d8
RadonZ <supachai27102000@gmail.com>
2023-05-28 04:32:35 +0700

    add photo check screen

--- frontend/Check-Photo/photocheckscreen.tsx
+++ frontend/Check-Photo/photocheckscreen.tsx
@@ -2,3 +2,4 @@ import React,{ useState } from "react";
 import {Alert,Text,Image,TextInput,View} from "react-native";
 
 import { RootStackScreenProps } from "../src/navigators/navigator-type";
+import axios from "axios"
