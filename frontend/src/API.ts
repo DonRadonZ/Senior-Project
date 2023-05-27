f8cb66de7ec9c36dd1b3c120b54fe604b4a48c0b
RadonZ <supachai27102000@gmail.com>
2023-05-28 04:32:44 +0700

    add API

---
+++ frontend/src/API.ts
@@ -0,0 +1,25 @@
+/* tslint:disable */
+/* eslint-disable */
+//  This file was automatically generated and should not be edited.
+
+
+export type Payment = {
+    __typename: "Payment",
+    account_id: number,
+    amount: number,
+    note: string,
+    
+}
+
+export type User = {
+    __typename: "User",
+    id: string,
+    email: string,
+    laser_id: string,
+    password: string,
+    address: string,
+    front_card_photo?: string | null,
+    back_card_photo ?: string | null
+
+}
+
