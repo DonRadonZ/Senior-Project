23a53ba52fc1e8a0c07c5fb2b4487c69303e8f17
RadonZ <supachai27102000@gmail.com>
2023-05-28 13:21:12 +0700

    add url

--- frontend/src/Screen/Register/register.tsx
+++ frontend/src/Screen/Register/register.tsx
@@ -64,6 +64,9 @@ export const RegisterScreen = () => {
     return <View style={styles.horizontalLine} />;
   };
 
+  const url = "http://127.0.0.1:5000/register";
+  
+
   return (
     <TouchableWithoutFeedback onPress={() => { Keyboard.dismiss() }}
     
