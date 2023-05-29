2d19641e863a755baad8a8c21908d5c0425fe343
RadonZ <supachai27102000@gmail.com>
2023-05-30 01:15:30 +0700

    update interface for mock

--- frontend/src/Components/helpers/interfaces.ts
+++ frontend/src/Components/helpers/interfaces.ts
@@ -1,8 +1,8 @@
 export interface User {
     id?: string;
     fullName: string;
-    avatar: string;
-    location?: string;
+    //avatar: string;
+    //location?: string;
 }
 
 export interface Services{
