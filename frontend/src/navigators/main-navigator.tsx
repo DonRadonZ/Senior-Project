910d2b6d7a85c816287b49a114dab9cbcc462624
RadonZ <supachai27102000@gmail.com>
2023-06-07 23:47:52 +0700

    update main navigator

--- frontend/src/navigators/main-navigator.tsx
+++ frontend/src/navigators/main-navigator.tsx
@@ -3,3 +3,16 @@ import {LinkingOptions,NavigationContainer} from "@react-navigation/native";
 import * as Linking from "expo-linking";
 import { AppNavigator } from './app-navigation';
 import { RootStackParams } from './navigator-type';
+
+const prefix = Linking.createURL("/");
+const linking: LinkingOptions<RootStackParams> = {
+    prefixes: [prefix, "savebuy://"],
+}
+
+export const MainNavigator = () => {
+    return(
+        <NavigationContainer>
+
+        </NavigationContainer>
+    )
+}
