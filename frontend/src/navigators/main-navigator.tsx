f9916b789df41af6ffd0f0d92a325b7412d53d3d
RadonZ <supachai27102000@gmail.com>
2023-06-07 23:30:31 +0700

    update main navigator

--- frontend/src/navigators/main-navigator.tsx
+++ frontend/src/navigators/main-navigator.tsx
@@ -1,2 +1,5 @@
 import React from 'react';
-import {LinkingOptions,NavigationContainer} from "@react-navigation/native"
+import {LinkingOptions,NavigationContainer} from "@react-navigation/native";
+import * as Linking from "expo-linking";
+import { AppNavigator } from './app-navigation';
+import { RootStackParams } from './navigator-type';
