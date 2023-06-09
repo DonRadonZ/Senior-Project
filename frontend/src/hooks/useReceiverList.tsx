8d1502b7af5a874b91eaf353e4f7b8d8cfd9a4e6
RadonZ <supachai27102000@gmail.com>
2023-06-10 01:15:24 +0700

    add query and hook

---
+++ frontend/src/hooks/useReceiverList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {useQuery} from '@apollo/cilent'
+
+const useReceiverList =() => {
+  return (
+    <div>useReceiverList</div>
+  )
+}
+
+export default useReceiverList
